8f86a8cf6a4cbeab6906ba1db7b52e80eb3486a8
Sefalo AF <sefaloaf@gmail.com>
2023-02-09 20:00:36 +0700

    perbaiki cart

---
+++ src/store/CartProvider.js
@@ -0,0 +1,6 @@
+import CartContext from "./Cart-context";
+const CartProvider = (props) => {
+  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+};
+
+export default CartProvider;
